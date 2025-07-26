52cc062c749af8ff21e0d67fae4aedfb36f80468
sanket531998 <sanket.naukarkar@gmail.com>
2025-07-27 00:15:57 +0530

    relayer code setup

---
+++ relayer/src/config.ts
@@ -0,0 +1,9 @@
+export const ETHEREUM = {
+  rpcUrl: process.env.ETH_RPC_URL!,
+  contractAddress: "0xYourEthAtomicSwapAddress",
+};
+
+export const APTOS = {
+  nodeUrl: process.env.APTOS_NODE_URL!,
+  contractAddress: "0xYourAptosModuleAddress",
+};
---
+++ relayer/src/index.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+// import { watchEth } from "./watchEth";
+
+// watchEth().catch(console.error);
